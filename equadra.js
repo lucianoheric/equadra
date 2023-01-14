ab56ca5b9eb0069d08f941c8a3d73023fc666a47
lucianoheric <lucianoheric55@gmail.com>
2023-01-14 15:01:32 -0300

    Inclusão primeiros Get e Post

--- equadra.js
+++ equadra.js
@@ -1,1 +1,17 @@
 const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/", (req,rep)=>{
+    rep.end("<h1>BEM VINDO</h1>")
+})
+
+app.post("/teste-post", (req,rep)=>{
+    let nome = req.body.name;
+    let email = req.body.email;
+    rep.send(nome + " - " + email);
+})
+
+app.listen(4000, ()=>{
+    console.log("Server running in port 4000");
+})
